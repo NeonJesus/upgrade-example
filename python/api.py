a5a6f61169668fad1b3b623ea6840f7846a3520b
Paul Walters <noirguy22@gmail.com>
2020-07-31 09:26:49 -0700

    ultimately, need to return a json

--- python/api.py
+++ python/api.py
@@ -1,6 +1,7 @@
 import flask
 import logging
 import sys
+import json
 from datetime import datetime
 
 handler = logging.StreamHandler(stream=sys.stdout)
@@ -9,8 +10,9 @@ logging.basicConfig(level=logging.DEBUG, handlers=[handler])
 
 def timestamp():
     logging.info('Request for Time received')
-    time = datetime.now()
+    time = {'date': datetime.now()}
     logging.info(f'Returning the time: {time}')
+    return json.dumps(time)
 
 
 if __name__ == '__main__':
