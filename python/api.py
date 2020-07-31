064f815e27f689baa17b50e2abffe335334f095e
Paul Walters <noirguy22@gmail.com>
2020-07-31 09:40:49 -0700

    use flask to start a simple webserver and return date time

--- python/api.py
+++ python/api.py
@@ -1,13 +1,17 @@
-import flask
 import logging
 import sys
 import json
+from flask import Flask
 from datetime import datetime
 
 handler = logging.StreamHandler(stream=sys.stdout)
 logging.basicConfig(level=logging.DEBUG, handlers=[handler])
 
 
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
 def timestamp():
     logging.info('Request for Time received')
     time = datetime.now()
@@ -17,4 +21,4 @@ def timestamp():
 
 
 if __name__ == '__main__':
-    timestamp()
+    app.run(debug=True)
